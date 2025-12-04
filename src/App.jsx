8bdc0591b10f8c94e412b7f82ea7136fbf94d6cc
ayeshayed18 <ayeshanasir28@gmail.com>
2025-12-04 17:07:45 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Portfolio from './assets/Portfolio'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <Portfolio/>
+    </>
+  )
+}
+
+export default App
